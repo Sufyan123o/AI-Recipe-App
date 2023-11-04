c232f0dde80fc7a8ada19d099546504e927f5d2f
shahardagan <shahar.dagan2@gmail.com>
2023-11-04 18:45:54 +0000

    UI improvements for home page
    Made by Sufyan

--- ingredients.py
+++ ingredients.py
@@ -1,20 +1,5 @@
 from taipy.gui import Gui, notify
 
-text = "Original text"
-
-# Definition of the page
-page = """
-# Ingredients Cart
-
-<|layout|columns=1 1|
-Ingredient
-
-<|X|button|on_action=delete_ingredient|>
-|>
-
-<|Generate Reciples|button|on_action=generate_recipes_clicked|>
-"""
-
 def display_ingredients(ingredientsArray):
     for ingredient in ingredientsArray:
         print(ingredient)
@@ -28,6 +13,23 @@ def generate_recipes_clicked(state):
 def delete_ingredient(state):
     notify(state, 'info', f'The text is: {state.text}')
     state.text = "Button Pressed"
+
+text = "Original text"
+
+# Definition of the page
+ingredients_md = """
+# Ingredients Cart
+
+<|layout|columns=1 1|
+Ingredient
+
+<|X|button|on_action=delete_ingredient|>
+|>
+
+<|Generate Reciples|button|on_action=generate_recipes_clicked|>
+"""
+
+
     
 
-Gui(page).run(use_reloader=True, port=8008)
+# Gui(page).run(use_reloader=True, port=8008)
