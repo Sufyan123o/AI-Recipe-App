f79a282b7b57e4ff6bc2d5c28b441592e29dfa3f
alishuh <alishanaseem@live.co.uk>
2023-11-04 18:39:15 +0000

    Ingredients - Alisha

--- ingredients.py
+++ ingredients.py
@@ -1,5 +1,21 @@
 from taipy.gui import Gui, notify
 
+def display_ingredients(ingredientsArray):
+    for ingredient in ingredientsArray:
+        print(ingredient)
+
+# Function to be called when the generate_recipes button is clicked
+def generate_recipes_clicked(state):
+    notify(state, 'info', f'The text is: {state.text}')
+    state.text = "Button Pressed"
+
+# Function to be called when the X button is clicked, it will delete the ingredient
+def delete_ingredient(state):
+    notify(state, 'info', f'The text is: {state.text}')
+    state.text = "Button Pressed"
+    
+
+
 text = "Original text"
 
 # Definition of the page
@@ -15,19 +31,5 @@ Ingredient
 <|Generate Reciples|button|on_action=generate_recipes_clicked|>
 """
 
-def display_ingredients(ingredientsArray):
-    for ingredient in ingredientsArray:
-        print(ingredient)
-
-# Function to be called when the generate_recipes button is clicked
-def generate_recipes_clicked(state):
-    notify(state, 'info', f'The text is: {state.text}')
-    state.text = "Button Pressed"
-
-# Function to be called when the X button is clicked, it will delete the ingredient
-def delete_ingredient(state):
-    notify(state, 'info', f'The text is: {state.text}')
-    state.text = "Button Pressed"
-    
 
 Gui(page).run(use_reloader=True, port=8008)
